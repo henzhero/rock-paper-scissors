8ff101bbb2358fd0acdeea23fa715977dd681823
henzhero <189972177+henzhero@users.noreply.github.com>
2025-01-30 14:55:44 +0800

    created function to get computer choice and prompts user their answer. added variables to track scores

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+function getHumanChoice() {
+    return prompt("Rock, Paper, or Scissors:");
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+/* create a function that plays a round (two para, human and comp choice)
+    turn human choice into case sensitive (lowercase)
+      compare human choice and comp choice */
+
+/* create a function that plays five rounds
+    add scores to the variables whoever wins or loses
+    after 5 rounds, declare winner */
+
+
