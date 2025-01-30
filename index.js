98c0675c56c736a4325e92cb3298e617408c060d
henzhero <189972177+henzhero@users.noreply.github.com>
2025-01-30 15:33:49 +0800

    created playRound function

--- index.js
+++ index.js
@@ -3,21 +3,25 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Rock, Paper, or Scissors:");
+    return prompt("Rock, Paper, or Scissors:").toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+const CHOICES = ["rock", "paper", "scissors"];
 
 let humanScore = 0;
 let computerScore = 0;
 
-/* create a function that plays a round (two para, human and comp choice)
-    turn human choice into case sensitive (lowercase)
-      compare human choice and comp choice */
+function playRound (computerChoice, humanChoice) {
 
-/* create a function that plays five rounds
-    add scores to the variables whoever wins or loses
-    after 5 rounds, declare winner */
+    if (humanChoice == CHOICES[0] && computerChoice == CHOICES[2]) {
+        humanScore++;
+    } else if (humanChoice == CHOICES[1] && computerChoice == CHOICES[0]) {
+        humanScore++;
+    } else if (humanChoice == CHOICES[2] && computerChoice == CHOICES[1]) {
+        humanScore++;
+    } else {
+        computerScore++;
+    }
+}
 
 
